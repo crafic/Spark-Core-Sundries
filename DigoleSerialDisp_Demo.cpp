75c0232323594e4d9a3214ca94edb95b310dc9d3
Timothy Brown <me@timb.us>
2014-02-22 22:14:15 -0500

    More useful setup instructions!

--- DigoleSerialDisp_Demo.cpp
+++ DigoleSerialDisp_Demo.cpp
@@ -15,18 +15,33 @@
 /* To use, call digole.begin(); *inside* void setup(); (or loop) to start the display. */
 /* You may also call digole.end(); to release the pins and clear the I2C/SPI/UART bus. */
 /*                                                                                     */
-/* Change XXXX in the #define below to the desired interface: SPI, SoftSPI, I2C, UART. */
 /* *********************************************************************************** */
 
-#define _Digole_Serial_XXXX_
+/* Uncomment the #define below for the desired interface: SPI, SoftSPI, I2C, UART. */
+
+//#define _Digole_Serial_SPI_
+//#define _Digole_Serial_SoftSPI_
+//#define _Digole_Serial_I2C_
+//#define _Digole_Serial_UART_
+
+/* Be sure to #include "DigoleSerialDisp.h" *after* the #define above or your compile will fail. */
 
 #include "DigoleSerialDisp.h"
 
-DigoleSerialDisp digole(arguments);
+/* Below are sane defaults for the various interfaces. Uncomment one and customize as needed! */
+
+//DigoleSerialDisp digole(A2); //SPI
+//DigoleSerialDisp digole(D4, D3, D2); //SoftSPI
+//DigoleSerialDisp digole(0x27); //I2C
+//DigoleSerialDisp digole(115200); //UART
 
 void setup() {
 	
 	digole.begin();
+	delay(500);
+	digole.clearScreen;
+	delay(50);
+	digole.print("Hello World!");
 
 }
 
